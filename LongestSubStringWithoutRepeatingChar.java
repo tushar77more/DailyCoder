487b5ee90b07a31410a77b42b1011dd69f83dfc4
tushar77more <33002579+tushar77more@users.noreply.github.com>
2021-05-23 15:19:02 +0530

    String operations

--- LongestSubStringWithoutRepeatingChar.java
+++ LongestSubStringWithoutRepeatingChar.java
@@ -6,7 +6,7 @@ public class LongestSubStringWithoutRepeatingChar {
 
 	public static void main(String[] args) {
 
-		String str="aaaaaaaaaaaaabbcdab";
+		String str="aaaaaaaaaaaaabbcdab"; 
 		getLongest(str);
 	}
 	
